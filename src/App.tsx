1d6085fdc06f6b54f9ce369bbc72dbb9fd94f875
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-02-23 19:30:07 +0530

    feat: created form to join or create a room

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useEffect, useRef } from "react";
-import Stream from "./components/Stream";
-import { useSocketContext } from "./context/Socket";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IFormInput {
+  roomId: string;
+}
 
 const App = () => {
-  const socket = useSocketContext();
-  const streamsContainerRef = useRef<HTMLDivElement | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<IFormInput>({ defaultValues: { roomId: "" } });
 
-  useEffect(() => {
-    if (!socket) return;
-
-    const handleNewStream = (newStream: MediaStream) => {
-      console.log("data coming", newStream);
-      const newVideo = document.createElement("video");
-      newVideo.srcObject = newStream;
-      streamsContainerRef.current &&
-        streamsContainerRef.current.appendChild(newVideo);
-      console.log(newVideo, newStream);
-    };
-    socket.on("newStream", handleNewStream);
-
-    return () => {
-      socket.off("newStream", handleNewStream);
-    };
-  }, [socket]);
+  // function to handle form submission
+  const handleFormSubmission: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div
-      ref={streamsContainerRef}
-      className="flex items-center w-screen h-screen gap-20 p-10"
-    >
-      <Stream />
+    <div className="flex items-center justify-center w-screen h-screen">
+      {/* form container */}
+      <form
+        onSubmit={handleSubmit(handleFormSubmission)}
+        className="flex flex-col gap-5 p-5 rounded-md shadow-md min-w-96"
+      >
+        <h1 className="text-xl font-medium text-center">
+          Connect with your loved one
+        </h1>
+
+        {/* for input box and error */}
+        <div className="w-full space-y-1">
+          <input
+            autoFocus
+            placeholder="Enter the room id"
+            className={`w-full px-2 py-1 border-2 rounded-md  ${
+              errors?.roomId
+                ? "focus-within:outline-red-500"
+                : "focus-within:outline-blue-500"
+            }`}
+            {...register("roomId", {
+              required: { value: true, message: "Please enter the room id" },
+              minLength: { value: 5, message: "Please enter a valid room id" },
+            })}
+          />
+          {/* for displaying the error */}
+          {errors?.roomId && (
+            <p className="text-sm text-red-500">* {errors?.roomId?.message}</p>
+          )}
+        </div>
+
+        {/* button to create a room */}
+        <button
+          disabled={isSubmitting}
+          type="submit"
+          className="py-2 font-medium text-white transition-all duration-300 ease-in-out bg-blue-600 border-2 border-blue-600 rounded-md hover:border-blue-500 hover:bg-blue-500"
+        >
+          Join a room
+        </button>
+
+        {/* for seprator */}
+        <div className="flex items-center gap-2">
+          {/* for line */}
+          <div className="w-full h-[2px] bg-black" />
+          <p className="font-medium">OR</p>
+          {/* for line */}
+          <div className="w-full h-[2px] bg-black" />
+        </div>
+
+        {/* button to create a room */}
+        <button
+          disabled={isSubmitting}
+          type="button"
+          className="py-2 font-medium text-blue-500 transition-all duration-300 ease-in-out border-2 border-blue-600 rounded-md hover:text-white hover:bg-blue-600"
+        >
+          Create a room
+        </button>
+      </form>
     </div>
   );
 };
